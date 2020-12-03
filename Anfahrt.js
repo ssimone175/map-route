2ac856295346d03efb21cafad547cc530cff326c
qlRitscher <ritscher@skouz.de>
2020-12-03 14:37:48 +0100

    Code Quality

--- Anfahrt.js
+++ Anfahrt.js
@@ -1,13 +1,8 @@
-
 import all from "https://js.api.here.com/v3/3.1/mapsjs.bundle.js";
-
 let tmpl = document.createElement('template');
-tmpl.innerHTML = `
-        <style>
-                *{
+tmpl.innerHTML = `<style>*{
                 font-size:1em;
                 }
-                
                 .form-group{
                     width:100%;
                     height:72px;
@@ -52,7 +47,6 @@ tmpl.innerHTML = `
                     .form-control{
                         width:70%;
                     }
-                
                 }
                 @media(max-width:270px){
                     button{
@@ -61,31 +55,23 @@ tmpl.innerHTML = `
                     .form-control{
                         width:60%;
                     }
-                
                 }
         </style>
         <div class="form-group">
         <input id="input" class="form-control" placeholder="Startpunkt">
         <button type="submit" id="button">Route starten</button>
         </div>`;
-
-class MapRoute extends HTMLElement {
-
-    constructor() {
-        super();
-        console.log("Anfahrt Component registered!");
-        let height = this.offsetHeight - 72;
-        const shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.innerHTML = `
+let mapTmp = document.createElement("template");
+mapTmp.innerHTML = `
             <style>
             :host{
                 width:100%;
                 position:relative;
-                z-index:1
+                z-index:1;
+                display:block;
             }
             #mapContainer{
                 width:100%;
-                height:` + height + `px;
                 position:relative;
                 z-index:-1;
             }
@@ -103,16 +89,19 @@ class MapRoute extends HTMLElement {
             }
             </style>
             <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
-            <map-input origin=` + this.origin + `></map-input>
+            <map-input></map-input>
             <div id="overlay"></div>
             <div id="mapContainer"></div>`;
-
+class MapRoute extends HTMLElement {
+    constructor() {
+        super();
+        console.log("Anfahrt Component registered!");
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.appendChild(mapTmp.content.cloneNode(true));
     }
-
     static get observedAttributes() {
         return ['origin', 'destination'];
     }
-
     get origin() {
         return this.getAttribute('origin');
     }
@@ -122,17 +111,32 @@ class MapRoute extends HTMLElement {
     get destination() {
         return this.getAttribute('destination');
     }
+    set destination(val){
+        this.setAttribute("destination",val);
+    }
     get apikey(){
         return this.getAttribute('apikey');
+    }
+    set apikey(val){
+        this.setAttribute("apikey",val);
     }
     get markerIcon(){
         return this.getAttribute('markerIcon');
     }
+    set markerIcon(val){
+        this.setAttribute("markerIcon",val);
+    }
     get lineColor(){
         return this.getAttribute('lineColor');
     }
+    set lineColor(val){
+        this.setAttribute("lineColor",val);
+    }
     get uiLayer(){
         return this.hasAttribute('uiLayer');
+    }
+    set uiLayer(val){
+        this.setAttribute("uiLayer",val);
     }
     get mapLayer(){
         if(this.getAttribute('mapLayer')=="Satellite"){
@@ -141,15 +145,18 @@ class MapRoute extends HTMLElement {
             return "normal";
         }
     }
-
+    set mapLayer(val){
+        this.setAttribute("mapLayer",val);
+    }
     connectedCallback(){
-
+        let height = this.offsetHeight - 72;
+        this.shadowRoot.getElementById("mapContainer").setAttribute("style", "height:" + height+"px");
+        this.shadowRoot.querySelector("map-input").setAttribute("origin", this.origin);
         if(!this.origin){
             let att = document.createAttribute('origin');
             att.value = " ";
             this.setAttributeNode(att);
         }
-
         var platform = new H.service.Platform({
             'apikey': this.apikey
         });
@@ -169,24 +176,18 @@ class MapRoute extends HTMLElement {
             this.origin = e.explicitOriginalTarget.value;
         });
     }
-
     handleInput(value){
         this.origin = value;
     }
-
     disconnectedCallback(){
         this.map.dispose();
     }
-
     createMap(centerPos){
         let platform = this.platform;
         // Retrieve the target element for the map:
         let targetElement = this.shadowRoot.getElementById("mapContainer");
-
         // Get the default map types from the platform object:
         let defaultLayers = platform.createDefaultLayers();
-
-
         // Instantiate the map:
         if(!centerPos){
             centerPos = {lat:52, lng: 8};
@@ -203,29 +204,20 @@ class MapRoute extends HTMLElement {
                 center: centerPos
             });
         this.map = map;
-
-
         if(this.uiLayer){
             var ui = H.ui.UI.createDefault(map, defaultLayers, "de-DE");
         }
-
         // Enable the event system on the map instance:
         var mapEvents = new H.mapevents.MapEvents(map);
-
         // Add event listeners:
         map.addEventListener('tap', function(evt) {
             // Log 'tap' and 'mouse' events:
             console.log(evt.type, evt.currentPointer.type);
         });
-
         // Instantiate the default behavior, providing the mapEvents object:
         var behavior = new H.mapevents.Behavior(mapEvents);
-
-
         this.drawRoute();
-
     }
-
     attributeChangedCallback(name, oldValue, newValue) {
         if (name === "origin" && oldValue!=newValue && this.mapCreated()) {
             this.removeMapObjectById(oldValue);
@@ -235,7 +227,6 @@ class MapRoute extends HTMLElement {
             this.drawRoute();
         }
     }
-
     mapCreated(){
         return this.platform && this.map;
     }
@@ -247,7 +238,6 @@ class MapRoute extends HTMLElement {
             }
         }
     }
-
     drawRoute(){
         let map = this.map;
         let platform = this.platform;
@@ -260,20 +250,18 @@ class MapRoute extends HTMLElement {
         if(this.lineColor){
             lineColor = this.lineColor;
         }else{
-            lineColor= 'black';
+            lineColor= 'blue';
         }
         if(this.origin.trim()){
             let end;
             let adr = this.origin;
             let start;
-
             let routingParameters = {
                 'routingMode': 'fast',
                 'transportMode': 'car',
                 // Include the route shape in the response
                 'return': 'polyline'
             };
-
             // Define a callback function to process the routing response:
             let onResult = function(result) {
                 // ensure that at least one route was found
@@ -284,43 +272,34 @@ class MapRoute extends HTMLElement {
                         this.shadowRoot.getElementById("overlay").innerHTML='<p class="overlay">'+duration+'</p>';
                         // Create a linestring to use as a point source for the route line
                         let linestring = H.geo.LineString.fromFlexiblePolyline(section.polyline);
-
                         // Create a polyline to display the route:
                         let routeLine = new H.map.Polyline(linestring, {
                             style: { strokeColor: lineColor, lineWidth: 3 }
                         });
                         routeLine.id = adr;
-
                         // Create a marker for the start point:
                         let startMarker = new H.map.Marker(section.departure.place.location, {icon:icon});
                         startMarker.id = adr;
-
                         // Create a marker for the end point:
                         let endMarker = new H.map.Marker(section.arrival.place.location, {icon:icon});
                         endMarker.id = adr;
-
                         // Add the route polyline and the two markers to the map:
                         map.addObjects([routeLine, startMarker, endMarker]);
-
                         // Set the map's viewport to make the whole route visible:
                         map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()});
                     });
                 }
             };
-
             // Get an instance of the routing service version 8:
             let router = platform.getRoutingService(null, 8);
-
             let calculateRoute = () => {
                 // Make sure that both destination and origin are present
                 if (!start || !end) return;
-
                 // Add origin and destination to the routing parameters
                 routingParameters.origin = start;
                 routingParameters.destination = end;
                 router.calculateRoute(routingParameters, onResult.bind(this), function(error) {alert(error.message);});
             }
-
             let service = platform.getSearchService();
             service.geocode({
                 q: adr
@@ -332,25 +311,20 @@ class MapRoute extends HTMLElement {
                     this.origin=" ";
                 }
             }, alert);
-
             service.geocode({
                 q: dest
             }, (result) => {
                 end = result.items[0].position.lat + ',' + result.items[0].position.lng;
                 calculateRoute();
             }, alert);
-
         }else if(this.destination.trim()){
-
             let goal;
-
             let makeMarker = () => {
                 let endMarker = new H.map.Marker(goal,{icon:icon});
                 endMarker.id = this.origin;
                 map.addObject(endMarker);
                 map.setCenter(goal);
             }
-
             let service = platform.getSearchService();
             service.geocode({
                 q: dest
@@ -359,25 +333,22 @@ class MapRoute extends HTMLElement {
                 makeMarker();
             }, alert);
             this.shadowRoot.getElementById("overlay").innerHTML="";
-
         }
     }
-
 }
 window.customElements.define('map-route', MapRoute);
-
 class MapInput extends HTMLElement {
-
     constructor() {
         super();
         console.log("Map-Input Component registered!");
         const shadowRoot = this.attachShadow({mode: 'open'});
         shadowRoot.appendChild(tmpl.content.cloneNode(true));
-
     }
-
     get origin() {
         return this.getAttribute('origin');
+    }
+    set origin(val){
+        this.setAttribute("origin",val);
     }
     connectedCallback(){
         this.shadowRoot.querySelector("button").onclick = this.handleClick.bind(this);
@@ -395,7 +366,5 @@ class MapInput extends HTMLElement {
         const input = this.shadowRoot.getElementById("input");
         input.dispatchEvent(new CustomEvent('map-input', {bubbles: true, composed: true}));
     }
-
-
 }
 window.customElements.define('map-input', MapInput);
